0b9cb40c3644caea2fd856d75c1b5329ddc5b4a5
TheGregCoder6789 <242897137+TheGregCoder6789@users.noreply.github.com>
2025-11-22 22:20:03 -0800

    cnaged for counter!

--- forhtmlproj1.js
+++ forhtmlproj1.js
@@ -1,1 +1,48 @@
-let count=0;const lc=document.getElementById("lc");function re(){lc.textContent=Count;}function dec(){count--;re();}function inc(){count++;re();}function res(){count=0;re();}re();
+let count = parseInt(localStorage.getItem("count")) || 0;
+const lc = document.getElementById("lc");
+
+const clickSound = new Audio("https://thegregcoder6789.github.io/TheGregCodersRepository1/click.mp3");
+const dingSound = new Audio("https://thegregcoder6789.github.io/TheGregCodersRepository1/ding.mp3");
+
+function re() {
+  lc.textContent = count;
+  localStorage.setItem("count", count);
+}
+
+function dec() {
+  count--;
+  re();
+  clickSound.play();
+  flash("red");
+  shakeScreen();
+}
+    
+function inc() {
+  count++;
+  re();
+  clickSound.play();
+  flash("green");
+  shakeScreen();
+}
+
+function res() {
+  count = 0;
+  re();
+  dingSound.play();
+  flash("yellow");
+  shakeScreen();
+}
+
+function flash(color) {
+  lc.classList.add(`flash-${color}`, `glow-${color}`);
+  setTimeout(() => {
+    lc.classList.remove(`flash-${color}`, `glow-${color}`);
+  }, 300);
+}
+
+function shakeScreen() {
+  document.body.classList.add("shake");
+  setTimeout(() => document.body.classList.remove("shake"), 400);
+}
+
+re();
